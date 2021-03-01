514c934c03cf1bf85420e77b742c92b3f0ac5209
BM_X_Coder <54219181+AbgegriftDH@users.noreply.github.com>
2021-03-01 14:03:30 +0100

    Update bot.js

--- discord/bot.js
+++ discord/bot.js
@@ -14,30 +14,3 @@ client.on('message', msg => {
 
 client.login('token');
 
-class ExecPHP {
-	/**
-	*
-	*/
-	constructor() {
-		this.phpPath = './discord/settings.php';
-		this.phpFolder = '';
-	}	
-	/**
-	*
-	*/
-	parseFile(fileName,callback) {
-		var realFileName = this.phpFolder + fileName;
-		
-		console.log('parsing file: ' + realFileName);
-
-		var exec = require('child_process').exec;
-		var cmd = this.phpPath + ' ' + realFileName;
-		
-		exec(cmd, function(error, stdout, stderr) {
-			callback(stdout);
-		});
-	}
-}
-module.exports = function() {
-	return new ExecPHP();
-};
